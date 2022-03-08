2464926f5b32a4a398409bbbfa353d2227b8b280
sarahhanlon <hanlons@go.stockton.edu>
2022-03-07 19:20:49 -0500

    working on hw2

--- HW2/MathGames.java
+++ HW2/MathGames.java
@@ -1,6 +1,6 @@
 package HW2;
 
-
+import java.util.Scanner;
 public class MathGames {
     // 1. Create a method that will calculate the Pythagorean Theorem (https://en.wikipedia.org/wiki/Pythagorean_theorem)
     // c = sqrt(a^2+b^2)
@@ -10,7 +10,6 @@ public class MathGames {
     // Hint 2: Math.pow and Math.sqrt are your friend here
     // Bonus points: Try to make it accept any 2 sides and return the 3rd. So it can take in
     // Hypotenuse and Perpendicular and return the base.
-
 
 
     
@@ -41,10 +40,16 @@ public class MathGames {
     
  
 
-
     // you do not need this main if you want to make a tester class
      public static void main(String[] args){
-        
+        Scanner side = new Scanner(System.in);
+        double base, perpendicular, hypotenuse;
+        System.out.println("Let's find the hypotenuse! What is the value of the base?");
+        base = side.nextDouble();
+        System.out.println("What is the value of the perpendicular?");
+        perpendicular = side.nextDouble();
+        hypotenuse = Math.sqrt((Math.pow(base, 2)) + (Math.pow(perpendicular, 2)));
+        System.out.println("The length of the hypotenuse is " + hypotenuse);
     }
 
 
