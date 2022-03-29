3fb332a84ea303dc1d1c7a0c63b4559a32aa6506
sarahhanlon <hanlons@go.stockton.edu>
2022-03-29 19:47:58 -0400

    HW2 Completed

--- HW2/MathGames.java
+++ HW2/MathGames.java
@@ -1,6 +1,7 @@
 package HW2;
 
 import java.util.Scanner;
+// import javax.sql.rowset.spi.SyncResolver;
 public class MathGames {
     // 1. Create a method that will calculate the Pythagorean Theorem (https://en.wikipedia.org/wiki/Pythagorean_theorem)
     // c = sqrt(a^2+b^2)
@@ -9,10 +10,9 @@ public class MathGames {
     //      Remember the Triangle has a base, Perpendicular, and Hypotenuse
     // Hint 2: Math.pow and Math.sqrt are your friend here
     // Bonus points: Try to make it accept any 2 sides and return the 3rd. So it can take in
-    // Hypotenuse and Perpendicular and return the base.
-
-
-    
+    public static String hypotenuse(double base, double perpendicular){
+        return "The length of the hypotenuse is: " + Math.sqrt((Math.pow(base, 2)) + (Math.pow(perpendicular, 2)));
+    }
     // 2. Create a method that will calcuate my grade in the class. You can use the grade range as follows
         // A: 100 - 90
         // B:  89 - 80
@@ -24,10 +24,28 @@ public class MathGames {
         // Hint 1: if statements are your friend here you will probably need at the least 5 if / ifelse / else statments
         // Hint 2: You will probably need to cast the double to an int
         // Hint 3: You will probably need to use the Math.ceil() method 
-
-
-
-
+        public static String gradeCalc(double avg){
+            
+            
+            if(avg >= 90){
+                return "A";
+            }
+            else if(avg >= 80 && avg < 90){
+                return "B";
+            }
+            else if(avg >= 70 && avg < 80){
+                return "C";
+            }
+            else if(avg >= 60 && avg < 70){
+                return "D";
+            }
+            else if(avg <= 60){
+                return "F";
+            }
+            else{
+                return "Error";
+           }
+        }
 
     // 3. Create a method that will calculate the tip on a bill per person
     //      Method parameters: total bill, amount of people splitting the bill, percentage to tip.
@@ -36,23 +54,57 @@ public class MathGames {
     //      Hint 2: Using doubles will be helpful for when you need to calculate decimals
     //      Hint 3: It is possible you might need to Cast a double to an int or vice versa
     //      Hint 4: You will probably need to use the Math.ceil() method 
-
     
- 
-
-    // you do not need this main if you want to make a tester class
-     public static void main(String[] args){
-        Scanner side = new Scanner(System.in);
-        double base, perpendicular, hypotenuse;
-        System.out.println("Let's find the hypotenuse! What is the value of the base?");
-        base = side.nextDouble();
-        System.out.println("What is the value of the perpendicular?");
-        perpendicular = side.nextDouble();
-        hypotenuse = Math.sqrt((Math.pow(base, 2)) + (Math.pow(perpendicular, 2)));
-        System.out.println("The length of the hypotenuse is " + hypotenuse);
+    
+    public static String tipCalc(double bill, double ppl, double percent){
+       
+        double myBill = Math.ceil(bill/ppl);
+        double tip = myBill * (percent/100);
+        return "You should tip $" + tip;
     }
 
 
+    // you do not need this main if you want to make a tester class
+     public static void main(String[] args){
+        Scanner choice = new Scanner(System.in);
+        System.out.println("Hello! would you like to (a) use the pythagorean theorem, (b) calculate your grade, or (c) calculate a tip?");
+        String input = choice.nextLine();
+        if(input.equalsIgnoreCase("a")){
+            Scanner side = new Scanner(System.in);
+            System.out.println("Let's find the hypotenuse! What is the value of the base?");
+            double base = side.nextDouble();
+            System.out.println("What is the value of the perpendicular?");
+            double perpendicular = side.nextDouble();
+            side.close(); 
+            System.out.println(hypotenuse(base, perpendicular));
+        }
+        else if(input.equalsIgnoreCase("b")){
+            Scanner grade = new Scanner(System.in);
+            System.out.println("what's your number grade?");
+            double avg = Math.ceil(grade.nextDouble());
+            grade.close();
+            System.out.println("The student's grade is: " + gradeCalc(avg));
+        }
+        else if(input.equalsIgnoreCase("c")){
+            Scanner info = new Scanner(System.in);
+            System.out.println("Let's find the tip :) What is your total bill?");
+            double bill = Math.ceil(info.nextDouble());
+            System.out.println("How many people are splitting the bill?");
+            int ppl = info.nextInt();
+            System.out.println("What percentage are you tipping?");
+            double percent = info.nextDouble();
+            info.close();
+            System.out.println(tipCalc(bill, ppl, percent));
+        }
+        else{
+            System.out.println("Error");
+        }
+        choice.close();
+     }
 
+        
 
+        
 }
+
+     
