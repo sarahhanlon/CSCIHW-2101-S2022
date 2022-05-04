d4dfa75194ac3f198b41a86e95d0a1daa212248f
sarahhanlon <hanlons@go.stockton.edu>
2022-05-04 17:38:26 -0400

    HW4 Complete

--- HW4/Arrays.java
+++ HW4/Arrays.java
@@ -1,5 +1,7 @@
 package HW4;
 
+import java.util.Scanner;
+
 public class Arrays {
 
     // CONVERT THE SUMS METHOD FROM HW3 TO ADD EACH VALUE TO AN ARRAY AND THEN OUTPUT THE 
@@ -23,8 +25,34 @@ public class Arrays {
     // Total: 22
     // Array: {12,2,3,4,1,0}
     public static void Arraysums(){
-
-    }
+        Scanner numb = new Scanner(System.in);
+        int sum = 0;
+        int i = 0;
+        int[] sumNumbers = new int[10];
+        int newNum = 1;
+        while (newNum != 0){
+            System.out.print("Number: ");
+            newNum = numb.nextInt();
+            sum += newNum;
+            sumNumbers[i] = newNum;
+            System.out.println("The total so far is: " + sum);
+            if (newNum == 0 || i == 9){
+                System.out.println("TOTAL ENDED --- The total is " + sum + ".");
+                System.out.print("Array: {");
+                for (int j = 0; j < 10; j++){
+                System.out.print(sumNumbers[j]+", ");
+                }
+                System.out.print("}");
+                break;
+            }
+        i++;
+        }
+            
+            
+            
+            
+        }
+    
 
     // Create a method that will brute force a password EX.
     // bruteForce("ZZZZ")
@@ -46,11 +74,22 @@ public class Arrays {
         char[] alphabet = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
         '!', '@', '#', '$', '%', '^', '&', '*', '(', ')','<','>','/','?'};
-    
+        char[] passArray = password.toCharArray();
+        int i = 0;
+        String yourPass = "";
+        while (i < (passArray.length)){
+            for (int j = 0; j < alphabet.length; j++){
+                if (passArray[i] == alphabet[j]){
+                    yourPass += alphabet[j];
+                    System.out.println(yourPass);
+                }
+            }
+            i++;
+        }
         return answer; 
     }
 
-    // Create a method that will sort an Array of integers from smallest to greatest.
+    // ..
     // NOTE: You CANNOT use the built in Array.sort method
     // Ex. 
     // sorter([9,10,2,5,3,4,7,8]) -> [2,3,4,5,7,8,9,10]
@@ -59,7 +98,22 @@ public class Arrays {
     // Hint 2: 2 nested For loops should be all thats needed for the swapping logic
     public static int[] sorter(int[] nums){
         int [] sortedArray = new int[nums.length];
-
+        System.out.print("Sorted array: [");
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++){
+                int temp = 0;
+                if (nums[i] > nums[j]){
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+         for (int i = 0; i < nums.length; i++) 
+           {     
+              System.out.print(nums[i] + ",");  
+           }
+           System.out.print("]");
         return sortedArray;
     }
 
@@ -67,7 +121,25 @@ public class Arrays {
 
 
     public static void main(String[] args) {
-
+        Scanner selection = new Scanner(System.in);
+        // this menu is to test them easier when im working on more than one
+        System.out.println("Hello, which method would you like to run? \n1: Arraysums \n2: bruteForce \n3: sorter");
+        int select = selection.nextInt();
+        if (select == 1){
+            System.out.println("I will add up the numbers you give me...");
+            Arraysums();
+        }else if (select == 2){
+            Scanner pass = new Scanner(System.in);
+            System.out.println("Enter password: ");
+            String password = pass.nextLine();
+            bruteForce(password);
+            pass.close();
+        }else if (select == 3){
+            int[] testArray = new int[]{9,10,2,5,3,4,7,8};
+            sorter(testArray);
+        }
+    selection.close();
+        
     }
 
     
